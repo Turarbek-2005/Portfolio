b25df448241f358de81bbd4f27e3d9766cf4cf2b
Turarbek-2005 <turarbek.kabdesh@mail.ru>
2024-07-29 02:59:01 +0500

    add bg to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="flex items-center bg">
+    <div class="flex bg">
         <div class="container mx-auto relative">
             <div class="z-10 lg:w-7/12" data-aos="fade-up-right">
                 <h2 class="text-4xl xl:text-5xl mb-4">
                     Привет! Меня зовут Турарбек!
                 </h2>
                 <p class="w-full text-xl xl:text-2xl mb-8">
-                    <!-- Я Junior Front-end программирующий на Vue (Nuxt) js. -->
                     Front-end Developer Vue (Nuxt) js
                 </p>
                 <p class="w-full text-lg xl:text-xl leading-8 xl:leading-9">
@@ -31,6 +30,10 @@
                 data-aos="fade-up-right"
                 class="hidden md:block absolute w-1/3 xl:right-40 sm:right-10 md:-top-10 sm:-top-20 -z-10 2xl:-top-20"
             />
+            <NuxtImg
+                src="/home-adaptive-bg.png"
+                class="block md:hidden absolute w-2/3 top-20 sm:top-0 right-10 -z-10 opacity-60"
+            />
         </div>
     </div>
 </template>
@@ -38,5 +41,6 @@
 <style scoped>
 .bg {
     height: calc(100vh - 10rem);
+    padding-top: 10%;
 }
 </style>
